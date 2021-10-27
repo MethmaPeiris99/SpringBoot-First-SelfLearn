cc27733ddc605a62a15a7457b65cd3dfc195dabb
Methma Peiris <methma34@gmail.com>
2021-10-27 19:44:28 +0530

    Initial commit, Add all default files

---
+++ springBoot-jsp-hello-world/src/main/java/com/example/springbootjsphelloworld/SpringBootJspHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjsphelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJspHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJspHelloWorldApplication.class, args);
+	}
+
+}
---
+++ springBoot-jsp-hello-world/src/test/java/com/example/springbootjsphelloworld/SpringBootJspHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjsphelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
